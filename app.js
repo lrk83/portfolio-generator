d281e761bf7b063affbdbbe12aefc8663526e426
Lukas Keel <lrk83@cornell.edu>
2021-07-01 16:07:05 -0600

    added css styling and replaced callbacks with promises

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const generateSite = require ('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
+const {writeFile, copyFile } = require("./utils/generate-site.js");
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -129,13 +130,20 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-        
-        fs.writeFile('./index.html', pageHTML, err =>{
-            if (err) throw new Error(err);
-
-            console.log("Page created! Checkout out index.html in this directory to see it!");
-        });
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
